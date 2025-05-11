ad38bd468c51ce8c45a4d9bf380b9e9080a66d3c
AngieGarciaRuiz <erosgabrielgarcia@gmail.com>
2025-05-10 19:05:29 -0500

    last version

--- ProyectoApi/obj/Debug/net6.0/ProyectoApi.AssemblyInfo.cs
+++ ProyectoApi/obj/Debug/net6.0/ProyectoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03adfe24ecd21bc679d093249f56f2c20af8b27e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d0a7e48f8637c727795cf9310227d6d58b6f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
